2d59977d7dde213b0cf337261868bfb4ccd9df77
Moriom Nela <moriomislamnela23@gmail.com>
2024-10-31 19:40:08 +0600

    Hospital Management C Project

---
+++ Hospital_Management.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+ struct patient{
+    int id;
+    char patientname[50];
+    char patientaddress[50];
+    char patientdisease[50];
+    char date[12];
+ }p;
+ struct doctor {
+    int id;
+    char name[50];
+    char address[50];
+    char specialize[50];
+    char date[12];
+ }d;
+
+
+
+
+
+
+
+
