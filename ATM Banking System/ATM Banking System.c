5e7b63001f7c69f370815d2487f90b1c7c662c25
Moriom Nela <moriomislamnela23@gmail.com>
2024-11-05 00:08:04 +0600

    ATM Banking System

---
+++ ATM Banking System/ATM Banking System.c
@@ -0,0 +1,68 @@
+#include<stdio.h>
+char name[20];
+int dip_amount;
+int amount=10000;
+int acc_no;
+
+void menu();
+int main(){
+  int choice;
+  printf("Enter your name:");
+  gets(name);
+
+  printf("Enter your account no: ");
+  scanf("%d",&acc_no);
+
+  menu();
+  printf("Enter your choice: ");
+  scanf("%d",&choice);
+
+  switch(choice){
+    case 1:
+       deposit_money();
+       break;
+    case 2:
+        withdraw_money();
+    case 3:
+        transfer_money();
+    case 4:
+        account_details();
+    case 5:
+        transaction_details();
+    case 6:
+        last_details();
+        exit(0);
+    default:
+        printf("Invalid to choice");
+        break;
+  }
+  return 0;
+}
+void menu(){
+   printf("MAIN MENU\n");
+   printf("1.Deposit Money\n");
+   printf("2.Withdraw Money\n");
+   printf("3.Transfer Money\n");
+   printf("4.Account details\n");
+   printf("5.Transaction details\n");
+   printf("6.Exit\n");
+}
+void deposit_money(){
+   printf("Deposit Money \n");
+   printf("enter the amount \n");
+   scanf("%d", &dip_amount);
+
+   amount += dip_amount;
+   printf("Money Deposit\n");
+   printf("Current balance : %d",amount);
+}
+
+
+
+
+
+
+
+
+
+
